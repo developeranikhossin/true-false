a9b915d3bbb00c2e2cb74008a683034200698224
developeranikhossin <mdmanunbiswas321@gmail.com>
2022-03-27 18:38:46 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,30 @@
+// falsy
+//0
+//""
+//undefined
+// null
+//NaN
+//truthy
+//'0' ' '
+// let nae = null;
+// console.log(nae);
+const name = false;
+// const age = 0;
+if (name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+
+
+
+
+
+
+
+
+
+
